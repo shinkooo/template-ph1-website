b26e9bb492e4a99d2615fc4a891f53916fe6123d
shuns <shunsei16710@icloud.com>
2023-01-28 11:36:38 +0900

    hennkou

--- quiz.page/answer.js
+++ quiz.page/answer.js
@@ -1,3 +1,10 @@
-function Q_(oxq){
- console;   
+'use strict';
+
+<link rel="stylesheet" href="style.css" />
+
+function process(){
+  document.getElementById('target1').textContent = 'すっごく真面目です！';  
+  document.getElementById('trigger1-1').classList.add('changed1');
 }
+
+document.getElementById('trigger1-1').addEventListener('click', process);
