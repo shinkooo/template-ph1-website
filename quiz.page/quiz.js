45d8c78f6bc80f21ebaef5e6d64cd993d34fedb9
shuns <shunsei16710@icloud.com>
2023-02-28 18:21:08 +0900

    week８まで終了

--- quiz.page/quiz.js
+++ quiz.page/quiz.js
@@ -1,5 +1,45 @@
 'use strict';
 
+// 難問目か、問題のタイトル、選択肢、補足っぽいやつをひとまとめにしたリストがほしい
+const QUESTIONS=[
+  {
+    index: 0,
+    title: "日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？",
+    choices: ["約28万人", "約79万人", "約183万人"],
+    note: "経済産業省 2019年3月 － IT 人材需給に関する調査",
+  },
+  {
+    index: 1,
+    title: "既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？",
+    choices: ["INTECH", "BIZZTECH", "X-TECH"],
+    note: "",
+  },
+  {
+    index: 2,
+    title: "IoTとは何の略でしょう？",
+    choices: ["INTInternet of Things", " Integrate into Technology ", "Information on Tool"],
+    note: "",
+  },
+  {
+    index: 3,
+    title: "イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？",
+    choices: ["Society 5.0", "CyPhy", "X-SDGs"],
+    note: "Society5.0 - 科学技術政策 - 内閣府",
+  },
+  {
+    index: 4,
+    title: "イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？",
+    choices: ["Web3.0", "NFT", "X-メタバース"],
+    note: "",
+  },
+  {
+    index: 5,
+    title: "先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？",
+    choices: ["約2倍", "約5倍", "X-約11倍"],
+    note: "Accenture Technology Vision 2021",
+  },
+]
+
 // 回答一覧
 const CORRECT_ANSWERS = [
   {
@@ -27,6 +67,74 @@ const CORRECT_ANSWERS = [
     value: '約5倍'
   }
 ];
+
+function addQuestions(){
+  const main= document.getElementById("js-quiz-in");
+
+  let content = "";
+
+  QUESTIONS.forEach(question => {
+
+
+    let choices = "";
+
+    question.choices.forEach((choice, index) =>{
+      choices += 
+      `
+      <li class="p-quiz-box__answer__item">
+        <button class="p-quiz-box__answer__button js-answer" data-answer="${index}">
+          ${choice}<img src="../assets/img/icon/icon-arrow.svg" alt="">
+        </button>
+      </li>
+      `
+    })
+
+
+    content += 
+    `
+      <section class="p-quiz-box js-quiz" data-quiz="${question.index}">
+      <div class="p-quiz-box__question">
+        <h2 class="p-quiz-box__question__title">
+          <span class="p-quiz-box__label">Q${question.index+1}</span>
+          <span class="p-quiz-box__question__title__text">${question.title}</span>
+        </h2>
+        <figure class="p-quiz-box__questioｃn__image">
+          <img src="../assets/img/quiz/img-quiz0${question.index+1}.png" alt="aaa">
+        </figure>
+      </div>
+      <div class="p-quiz-box__answer">
+        <span class="p-quiz-box__label p-quiz-box__label--accent">A</span>
+        <ul class="p-quiz-box__answer__list">
+    `
+      + choices +
+    `
+        </ul>
+        <div class="p-quiz-box__answer__correct js-answerBox">
+          <p class="p-quiz-box__answer__correct__title js-answerTitle"></p>
+          <p class="p-quiz-box__answer__correct__content">
+            <span class="p-quiz-box__answer__correct__content__label">A</span>
+            <span class="js-answerText"></span>
+          </p>
+        </div>
+      </div>
+    `;
+
+    if (question.note!="") {
+      content += 
+      `
+        <cite class="p-quiz-box__note">
+          <img src="../assets/img/icon/icon-arrow.svg" alt="" class="">${question.note}
+        </cite>
+      `
+    };
+
+    content += ` </section>`;
+  })
+
+  main.insertAdjacentHTML("beforeend", content);
+}
+
+addQuestions();
 
 // 問題ごと
 const allQuiz = document.querySelectorAll(".js-quiz");
